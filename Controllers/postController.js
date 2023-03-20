fdbf22250367698b1eb78438b042e45df2c1fa07
zeyadelshaarawy <shaarawyziad121@gmail.com>
2023-03-21 00:33:08 +0200

    data modelling

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -21,9 +21,7 @@ exports.getPost = async (req, res) => {
   try {
     try {
       const post = await Post.findById(paramId);
-      res.status(200).json({
-        data: post,
-      });
+      res.status(200).json(post);
     } catch (err) {
       res.status(404).json({
         message: "POST DOES NOT EXISTS",
