48e1eb9e00ae13f568857c8b0b46b782cdc5620a
zeyadelshaarawy <shaarawyziad121@gmail.com>
2023-03-21 00:29:27 +0200

    updated cors4

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -4,10 +4,10 @@ const postController = require("../Controllers/postController");
 const router = express.Router();
 //creating a post
 router.post("/", postController.createPost);
-//get single post
-router.get("/", postController.getPosts);
 //get all posts
-router.get("/:id", postController.getPosts);
+router.get("/", postController.getPosts);
+//get single post
+router.get("/:id", postController.getPost);
 //edit post
 router.put("/:id", postController.editPost);
 
